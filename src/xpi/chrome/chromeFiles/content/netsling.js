2bb25ec8df6e0071e21990e1cf8e9167d81317c7
Chris Wilper <cwilper@gmail.com>
2007-02-01 22:33:04 +0000

    mostly works, still need special add command

--- src/xpi/chrome/chromeFiles/content/netsling.js
+++ src/xpi/chrome/chromeFiles/content/netsling.js
@@ -4,6 +4,16 @@ function netsling(event) {
     }
 }
 
+var prefs = Components.classes["@mozilla.org/preferences-service;1"].
+                    getService(Components.interfaces.nsIPrefService).
+                    getBranch("extensions.netsling.");
+
+var firstRun = prefs.getBoolPref("firstRun");
+if (firstRun) {
+    alert("Thanks for installing NetSling!\n\nRemember: The hotkey is CTRL-SPACE.");
+    prefs.setBoolPref("firstRun", false);
+}
+
 var urls = new Array();
 
 window.addEventListener("keypress", netsling, false);
--- src/xpi/defaults/preferences/prefs.js
+++ src/xpi/defaults/preferences/prefs.js
@@ -1,1 +1,4 @@
-pref("extensions.netsling.delicious.username", "");
+pref("extensions.netsling.firstRun", true);
+pref("extensions.netsling.alwaysNewPage", false);
+pref("extensions.netsling.useTabs", false);
+pref("extensions.netsling.command.hey", "http://cnn.com/");
